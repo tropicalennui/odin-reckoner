ee544ea71d630a65d4acbe75d08bdbdc269c2520
Rosemary <rjingrey@gmail.com>
2022-09-13 08:38:45 +1000

    clicker function is storing values for operator function

--- script.js
+++ script.js
@@ -6,6 +6,38 @@ function operate(num1, operator, num2) {
 					: 'ERROR';
 }
 
+let firstNum = 0;
+let secondNum = 0;
+let action = '';
+
+function clicker(e) {
+	const operators = ['+', '-', '*', '/'];
+
+	if (action == '' && !Number.isNaN(parseInt(e.target.id))) {
+		if (firstNum == 0) {
+			firstNum = e.target.id;
+		} else {
+			firstNum += e.target.id;
+		}
+		input = firstNum;
+	}
+
+	if (operators.includes(e.target.id)) {
+		action = e.target.id;
+		input = firstNum + ' ' + action;
+	}
+
+	if (firstNum != '' && action != '' && !Number.isNaN(parseInt(e.target.id))) {
+		if (secondNum == 0) {
+			secondNum = e.target.id;
+		} else {
+			secondNum += e.target.id;
+		}
+		input = firstNum + ' ' + action + ' ' + secondNum;
+	}
+	inputDiv.innerText = `${input}`;
+}
+
 let result = '';
 const resultClear = '0';
 const resultDiv = document.getElementById('result');
@@ -18,10 +50,7 @@ inputDiv.innerText = `${inputClear}`;
 
 const calcButtonDivs = document.querySelectorAll('.calc-button');
 calcButtonDivs.forEach(calcButtonDiv => {
-	calcButtonDiv.addEventListener('click', function (e) {
-		input += e.target.id;
-		inputDiv.innerText = `${input}`;
-	});
+	calcButtonDiv.addEventListener('click', clicker);
 });
 
 const clearButton = document.getElementById('clr');
