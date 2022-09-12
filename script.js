cfeb0667e074ff9c52aebd64d1e5537b2a3fce8a
Rosemary <rjingrey@gmail.com>
2022-09-13 07:03:21 +1000

    input displayed after click

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ function operate(num1, operator, num2) {
 				: operator == '/' ? num1 / num2
 					: 'ERROR';
 }
+
+let result = '8008S';
+const resultDiv = document.getElementById('result');
+resultDiv.innerText = `${result}`;
+
+let input = '';
+const inputDiv = document.getElementById('input');
+
+const calcButtonDivs = document.querySelectorAll('.calc-button');
+calcButtonDivs.forEach(calcButtonDiv => {
+	calcButtonDiv.addEventListener('click', function (e) {
+		input += e.target.id;
+		inputDiv.innerText = `${input}`;
+	});
+});
+
+const clearButton = document.getElementById('clr');
+clearButton.addEventListener('click', function (e) {
+	input = '';
+	inputDiv.innerText = `${input}`;
+});
