052a4fa31c33094e02792f8519aaffd56965f8d8
Rosemary <rjingrey@gmail.com>
2022-09-14 12:04:26 +1000

    fixed issue with firstNum being zero breaking the operator divs

--- script.js
+++ script.js
@@ -30,27 +30,27 @@ function operate(num1, operator, num2) {
 function interact(clickedDiv) {
 	const operators = ['+', '-', '*', '/'];
 
-	if (action == '' && !Number.isNaN(parseInt(clickedDiv.target.id))) {
+	if (action === '' && !Number.isNaN(parseInt(clickedDiv.target.id))) {
 		//store the first number until an operator is selected
-		if (firstNum == '') {
+		if (firstNum === '') {
 			firstNum = clickedDiv.target.id;
 		} else {
 			firstNum += clickedDiv.target.id;
 		}
 		input = firstNum;
-	} else if (operators.includes(clickedDiv.target.id) && firstNum != '' && secondNum == '') {
+	} else if (operators.includes(clickedDiv.target.id) && firstNum !== '' && secondNum === '') {
 		//store the last operator clicked until second number is selected
 		action = clickedDiv.target.id;
 		input = firstNum + ' ' + action;
-	} else if (firstNum != '' && action != '' && !Number.isNaN(parseInt(clickedDiv.target.id))) {
+	} else if (firstNum !== '' && action !== '' && !Number.isNaN(parseInt(clickedDiv.target.id))) {
 		//store the second number until another operator or equals is selected
-		if (secondNum == '') {
+		if (secondNum === '') {
 			secondNum = clickedDiv.target.id;
 		} else {
 			secondNum += clickedDiv.target.id;
 		}
 		input = firstNum + ' ' + action + ' ' + secondNum;
-	} else if (operators.includes(clickedDiv.target.id) && secondNum != '') {
+	} else if (operators.includes(clickedDiv.target.id) && secondNum !== '') {
 		//operate the two numbers and save as firstNum
 		firstNum = operate(parseInt(firstNum), action, parseInt(secondNum));
 		action = clickedDiv.target.id;
